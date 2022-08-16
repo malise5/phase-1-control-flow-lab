0a17e8649e9f45baf24b0f2fdcfc9768142d9c70
Halkano <kudezx@gmail.com>
2022-08-16 18:14:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400){
+    return "This one is on me!";
+  }
+  else if (feet > 2000 && feet < 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (feet > 2500){
+    return 'No can do.';
+  }
 }
+console.log(scuberGreetingForFeet(199));
+console.log(scuberGreetingForFeet(2001));
+console.log(scuberGreetingForFeet(2501));
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go."
 }
+console.log(ternaryCheckCity("NYC"));
+console.log(ternaryCheckCity("Pittsburgh"));
+
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch(tip){
+    case "generous":
+      return 'Thank you so much.';
+      break;
+    case "not as generous":
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.'
+
+  }
+}
+console.log(switchOnCharmFromTip("generous"));
+console.log(switchOnCharmFromTip("not as generous"));
+console.log(switchOnCharmFromTip("thanks for everythings"));
